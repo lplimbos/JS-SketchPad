7651299ec993b6ced7e0285c8641a42818553bd0
lplimbos <lplimbos@gmail.com>
2017-03-08 13:45:25 -0600

    Added a clear button, made some changes to the general logic of the script

--- scripts/script.js
+++ scripts/script.js
@@ -6,35 +6,58 @@ var $rowClose;
 var colorBlack = '#000000';
 var currentColor = colorBlack;
 var currentSize = 32;
+var drawing = false;
 
-//We make the size of the canvas to be the exact same width and height as the sum of all the blocks
-$('#canvas').css('width', ((Math.ceil(800 / currentSize))*currentSize) + 'px');
-$('#canvas').css('height', ((Math.ceil(600 / currentSize))*currentSize) + 'px');
+//We draw the canvas with default settings
+makeCanvas(800, 600, currentSize);
 
-//We fill out the canvas with the rows that will hold our blocks
-for(var i=0; i<currentSize; i++){
-	$rowInit = $("<div class='rowContainer'>");
-	$('#canvas').append($rowInit);
-}
-//We make the rows be the exact height we need to fill our canvas
-$('.rowContainer').css('height', Math.ceil(600 / currentSize) + 'px');
-
-//This is used for the same reason as the rows but instead we fill all the rows with our blocks(at the same time!)
-for(var j=0; j<currentSize; j++){
-	$block = $("<div class='block'></div>");
-	$('.rowContainer').append($block);
-}
-//The blocks are then fitted to the canvas perfectly
-$('.block').css('width', Math.ceil(800 / currentSize) + 'px');
+$('#canvas').on('click', function(){
+	drawing = !drawing;
+});
 
 //This is the main painting tool, the color is changed here
 $('.block').on('mouseenter', function(){
-	$(this).css('background-color', 'currentColor');
+	if (drawing){
+		$(this).css('background-color', 'currentColor');
+	}
 });
 
+$('#clearButton').on('click', function(){
+	$('#canvas').find('.block').css('background-color', '#ffffff');
+});
 //I use this mainly for debugging purposes, these returns the blocks we exit the mouse out of to their regular color
 /*$('.block').on('mouseleave', function(){
 	$(this).css('background-color', '#ffffff');
 });*/
+
+
+
+
+
+
+
+//This function is what redraws the canvas in case of any changes
+function makeCanvas(width, height, size){
+//We make the size of the canvas to be the exact same width and height as the sum of all the blocks
+$('#canvas').css('width', ((Math.ceil(width / size))*size) + 'px');
+$('#canvas').css('height', ((Math.ceil(height / size))*size) + 'px');
+
+//We fill out the canvas with the rows that will hold our blocks
+for(var i=0; i<size; i++){
+	$rowInit = $("<div class='rowContainer'>");
+	$('#canvas').append($rowInit);
+}
+//We make the rows be the exact height we need to fill our canvas
+$('.rowContainer').css('height', Math.ceil(height / size) + 'px');
+
+//This is used for the same reason as the rows but instead we fill all the rows with our blocks(at the same time!)
+for(var j=0; j<size; j++){
+	$block = $("<div class='block'></div>");
+	$('.rowContainer').append($block);
+}
+//The blocks are then fitted to the canvas perfectly
+$('.block').css('width', Math.ceil(width / size) + 'px');
+$('.block').css('height', Math.ceil(height / size) + 'px');
+}
 
 });
